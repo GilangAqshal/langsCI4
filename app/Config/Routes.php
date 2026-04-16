7650c20ccb1f14ef5da417b61bd53a5265fbd195
GilangAqshal <gilangais16@gmail.com>
2026-04-16 16:44:23 +0700

    feat: add layout and pages structure in CI4 views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,9 +10,13 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->setAutoRoute(true);
 // $routes->get('/', 'Home::index');
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Pages::index');
+$routes->get('pages/about', 'Pages::about');
+
+// belajar segment
 $routes->get('/coba/index','Coba::index');
 $routes->get('/coba/(:any)/(:num)', 'Coba::about/$1/$2');
-
+// belajar controller folder 
 $routes->get('/users', 'Admin\users::index');
-$routes->get('/users', 'Admin\users::badmin');
+$routes->get('/user/badmin', 'Admin\users::badmin');
---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+    <div class="container">
+        <div class="row">
+            <div class="cloumn">
+                <h1>About me</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam incidunt ipsum accusantium nostrum aspernatur corporis blanditiis consectetur officiis sequi inventore, nam quas voluptatum necessitatibus tempora dolor ullam molestiae! Eaque, dolores.</p>
+
+            </div>
+        </div>
+    </div>
